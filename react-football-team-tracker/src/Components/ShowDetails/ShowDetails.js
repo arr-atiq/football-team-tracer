e42d27e5d94c29282a8b4ab234d4afc73e60d76e
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-11 15:43:31 +0600

    showed team details

--- react-football-team-tracker/src/Components/ShowDetails/ShowDetails.js
+++ react-football-team-tracker/src/Components/ShowDetails/ShowDetails.js
@@ -1,25 +1,28 @@
-// import React from 'react';
-// import './ShowDetails.css'
+import React from 'react';
+import './ShowDetails.css'
 
-// const ShowDetails = (props) => {
-//     const { strTeam, intFormedYear, strLeague, strStadium, strCountry, strTeamBanner, strGender } = props.playerInfo
-//     return (
-//         <div className="set-team-info">
-//             <div className="team-data">
-//                 <div className="data-detail">
-//                     <h5>{strTeam}</h5>
-//                     <h5>{strLeague}</h5>
-//                     <h5>{strStadium}</h5>
-//                     <h5>{intFormedYear}</h5>
-//                     <h5>{strCountry}</h5>
-//                     <h5>{strGender}</h5>
-//                 </div>
-//                 <div className="banner">
-//                     <img src={strTeamBanner} alt="" />
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+const ShowDetails = (props) => {
+    const { strTeam, intFormedYear, strLeague, strStadium, strCountry,strDescriptionEN  , strTeamBanner, strGender } = props.playerInfo
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-5 main-card">
+                    <h5>{strTeam}</h5>
+                    <h5>{strLeague}</h5>
+                    <h5>{strStadium}</h5>
+                    <h5>{intFormedYear}</h5>
+                    <h5>{strCountry}</h5>
+                    <h5>{strGender}</h5>
+                </div>
+                <div className="col-md-5 img-banner">
+                    <img src={strTeamBanner} alt="" />
+                </div>
+            </div>
+            <div className="para-div">
+            <p>{strDescriptionEN }</p>
+            </div>
+        </div>
+    );
+};
 
-// export default ShowDetails;
+export default ShowDetails;
--- react-football-team-tracker/src/Components/TeamDetails/TeamDetails.js
+++ react-football-team-tracker/src/Components/TeamDetails/TeamDetails.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import ShowDetails from '../ShowDetails/ShowDetails';
 import './TeamDetails.css';
 
 const TeamDetails = () => {
     const {idTeam} = useParams();
     // console.log({idTeam});
-    const [info, setInfo] = useState({});
-    console.log(info);
+    const [info, setInfo] = useState([]);
     useEffect(()=>{
         const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`;
         console.log(url);
@@ -16,7 +16,9 @@ const TeamDetails = () => {
     },[idTeam])
     return (
         <div>
-            <h5>i have got it : {idTeam}</h5>
+            {
+                info.map(playerInfo => <ShowDetails playerInfo={playerInfo}></ShowDetails>)
+            }
         </div>
     );
 };
